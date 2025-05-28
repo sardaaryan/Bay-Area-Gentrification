21342d2fbd9b1b5f1e06f4461580972de55e2aa6
Aryan Sarda <asarda@hvantage.com>
2025-05-28 16:59:47 -0700

    topojson preprocessing

--- map_preprocess/counties/filter.js
+++ map_preprocess/counties/filter.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 
 // Combined FIPS code list
-const bayAreaCountyIDs = ["06001", "06013", "06041", "06055", "06075", "06081", "06085", "06095", "06097"];
-const mapCountyIDs = ["06087", "06053", "06069", "06047", "06099", "06077", "06009", "06005", "06017", "06067", "06113", "06101", "06011", "06033", "06045", "06061", "06057", "06115"];
-const targetIDs = new Set([...bayAreaCountyIDs, ...mapCountyIDs]);
+const targetIDs = new Set(["06001", "06013", "06041", "06055", "06075", "06081", "06085", "06095", "06097"]);
+//const mapCountyIDs = ["06087", "06047", "06099", "06077", "06009", "06005", "06017", "06067", "06113", "06101", "06011", "06033", "06045", "06061", "06057", "06115"];
+//const targetIDs = new Set([...bayAreaCountyIDs]);
 
 // Load the original TopoJSON
 const topojson = JSON.parse(fs.readFileSync("cali.json", "utf8"));
@@ -15,10 +15,8 @@ if (!topojson.objects || !topojson.objects.counties) {
 }
 
 const originalCounties = topojson.objects.counties.geometries;
-//console.log(originalCounties);
 const filteredCounties = originalCounties.filter((geom) => targetIDs.has(geom.properties.id));
 
-console.log(filteredCounties.length);
 
 // Replace with filtered counties
 topojson.objects.counties.geometries = filteredCounties;
